acdffd7b4830637b9dd92386131b99ebe8e76902
Mughees Javed <mugheesk80@gmail.com>
2024-02-03 21:42:13 +0500

    ready to deploying

--- route.js
+++ route.js
@@ -164,7 +164,8 @@ app.use((err, req, res, next) => {
 
   res.status(status).render("error", { err });
 });
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Serving on port 3000");
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
 });
